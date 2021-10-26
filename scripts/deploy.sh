120e5dcab12c653d991ddf1a08318c804dcc22f2
peterajessup <peterajessup@users.noreply.github.com>
2021-10-27 10:11:33 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,19 +3,17 @@ oc project mq
 set +e
 # Remove the runnning queue manager instance (if any)
 
-oc delete QueueManager mq1
+oc delete QueueManager yalta-queue-manager
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route mq1route
-oc delete route mq1routercvr
-oc delete secret mq1key
-oc delete configMap mq1-mqsc
-oc delete persistentvolumeclaim data-mq1-ibm-mq-0 data-mq1-ibm-mq-1 data-mq1-ibm-mq-2
+oc delete route yaltaroute
+oc delete secret yaltakey
+oc delete configMap yalta-mqsc
+oc delete persistentvolumeclaim 
 set -e
 # Create the route and the keystore secret and mqsc configMap
-oc apply -f mq1Route.yaml
-oc apply -f routeMQ1rcvr.yaml
-oc create secret tls mq1key --cert=./tls/tls.crt --key=./tls/tls.key
+oc apply -f yaltaRoute.yaml
+oc create secret tls yaltakey --cert=./tls/tls.crt --key=./tls/tls.key
 oc create -f mqsc/mqsc.yaml
 
 set -e
